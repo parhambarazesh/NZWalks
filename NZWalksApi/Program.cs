043178677474f3bc971f16750509e8c44fdcfb3e
parhambarazesh <parham.barazesh@gmail.com>
2022-11-07 01:33:33 +0100

    inject IRegionRepository and AutoMapper

--- NZWalksApi/Program.cs
+++ NZWalksApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalksApi.Data;
+using NZWalksApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<NZWalksDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks")));
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
